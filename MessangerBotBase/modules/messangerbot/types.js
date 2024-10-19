4a8c45cdeb2fad82c695a3bbe9e628f4ad0d85d7
job-gut <104257231+job-gut@users.noreply.github.com>
2024-10-19 19:17:55 +0900

    Delete MessangerBotBase directory

--- MessangerBotBase/modules/messangerbot/types.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// These types are defined for MESSANGER BOT
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidHlwZXMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJ0eXBlcy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUEsNENBQTRDIn0=
